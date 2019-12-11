26040e5da1334de608531946e876af31e411940b
Rowena Chen <rwnchen@gmail.com>
2019-12-11 17:42:14 -0500

    finished last two exercises

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,9 +2,20 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  let currentNode = linkedlist.head;
+  let dictionary = {};
+  while (currentNode.next) {
+    const value = currentNode.value;
+    if (dictionary[value]) {
+      if (dictionary[value].includes(currentNode)) return true;
+      dictionary[value] = [...dictionary[value], currentNode];
+    }
+    dictionary[value] = [currentNode];
 
+    currentNode = currentNode.next;
+  }
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +25,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,32 @@
 'use strict';
 
 // Complete this algo
-const minJumps = arr => {
+const minJumps = (arr) => {
+  let jumps = 0;
 
+  let oneJumpTo = [arr.length - 1];
+
+  let found = false;
+  while (!found) {
+    jumps++;
+    let newGoals = [];
+
+    oneJumpTo.forEach((goal) => {
+      for (let i = 0; i < goal; i++) {
+        if (i + arr[i] >= goal) {
+          if (i === 0) {
+            found = true;
+          }
+          newGoals.push(i);
+        }
+      }
+    });
+
+    oneJumpTo = newGoals;
+    console.log(oneJumpTo);
+  }
+
+  return jumps;
 };
 
-module.exports = minJumps
+module.exports = minJumps;
